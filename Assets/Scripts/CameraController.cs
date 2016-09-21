d75a3d8134c651c92fbf864d2389e6270db4b13b
SidneyFanFan <SidneyFan716@gmail.com>
2016-09-20 23:43:11 -0700

    Init commit

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour
+{
+
+	public GameObject player;
+
+	// Use this for initialization
+	void Start ()
+	{
+		cameraOn (player);
+	}
+
+	// LateUpdate is called after Update each frame
+	void LateUpdate ()
+	{
+		cameraOn (player);
+	}
+
+	void cameraOn (GameObject obj)
+	{
+//		transform.position += new Vector3 (obj.transform.position.x, obj.transform.position.y, 0);
+		transform.position = new Vector3 (obj.transform.position.x, obj.transform.position.y, transform.position.z);
+	}
+}
---
+++ Assets/Scripts/FireBallScript.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FireBallScript : MonoBehaviour
+{
+
+	public float speed = 6;
+	public AudioClip shootAudio;
+	public AudioClip hitAudio;
+
+	private Rigidbody2D rb2d;
+	private bool shouldBeDestroied = false;
+	private AudioSource audioSource;
+
+	// Use this for initialization
+	void Start ()
+	{
+		rb2d = GetComponent<Rigidbody2D> ();
+		rb2d.velocity = new Vector2 (0, 0);
+		audioSource = GetComponent<AudioSource> ();
+		audioSource.PlayOneShot (shootAudio);
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+		if (!audioSource.isPlaying && shouldBeDestroied) {
+			Destroy (gameObject);
+		}
+	}
+		
+	// Function called when the enemy collides with another object
+	void OnTriggerEnter2D (Collider2D obj)
+	{
+		if (shouldBeDestroied)
+			return;
+		if (obj.CompareTag ("Element") && !obj.name.Equals ("FireElement")) {
+			audioSource.PlayOneShot (hitAudio);
+			GetComponent <Renderer> ().enabled = false;
+			shouldBeDestroied = true;
+		}
+	}
+}
